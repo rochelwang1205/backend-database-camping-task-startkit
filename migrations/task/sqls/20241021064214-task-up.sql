9a48fc64738857221207a3ccaf4609f4bfc3a8a7
Rochel <liuyingfeng57@gmail.com>
2024-11-29 13:41:25 +0800

    update task3

--- migrations/task/sqls/20241021064214-task-up.sql
+++ migrations/task/sqls/20241021064214-task-up.sql
@@ -6,105 +6,7 @@
 --   █ █   █████ █   █     █ 
 -- ===================== ====================
 -- 1. 用戶資料，資料表為 USER
-CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
-CREATE TABLE "USER" (
-  "id" uuid PRIMARY KEY NOT NULL DEFAULT (gen_random_uuid()),
-  "name" varchar(50) NOT NULL,
-  "email" varchar(320) UNIQUE NOT NULL,
-  "role" varchar(20) NOT NULL,
-  "created_at" timestamp NOT NULL DEFAULT (CURRENT_TIMESTAMP),
-  "updated_at" timestamp NOT NULL DEFAULT (CURRENT_TIMESTAMP)
-);
-CREATE TABLE "SKILL" (
-  "id" uuid PRIMARY KEY DEFAULT uuid_generate_v4(),
-  "name" varchar(50) UNIQUE NOT NULL,
-  "created_at" timestamp NOT NULL DEFAULT (CURRENT_TIMESTAMP)
-);
 
-INSERT INTO "SKILL" (name) VALUES ('重訓'), ('瑜伽'), ('有氧運動'), ('復健訓練');
-
-CREATE TABLE "COACH" (
-  "id" uuid PRIMARY KEY NOT NULL DEFAULT uuid_generate_v4(),
-  "user_id" uuid NOT NULL REFERENCES "USER"(id),
-  "experience_years" integer,
-  "description" text,
-  "profile_image_url" varchar(2048),
-  "created_at" timestamp NOT NULL DEFAULT (CURRENT_TIMESTAMP),
-  "updated_at" timestamp NOT NULL DEFAULT (CURRENT_TIMESTAMP),
-  UNIQUE("user_id")
-);
-
-CREATE TABLE "COACH_LINK_SKILL" (
-  "coach_id" uuid NOT NULL REFERENCES "COACH"(id),
-  "skill_id" uuid NOT NULL REFERENCES "SKILL"(id),
-  "created_at" timestamp NOT NULL DEFAULT (CURRENT_TIMESTAMP),
-  PRIMARY KEY ("coach_id", "skill_id")
-);
-
-CREATE TABLE "CREDIT_PACKAGE" (
-  "id" serial PRIMARY KEY,
-  "name" varchar(50) NOT NULL,
-  "credit_amount" integer NOT NULL,
-  "price" numeric(10,2) NOT NULL,
-  "created_at" timestamp NOT NULL DEFAULT (CURRENT_TIMESTAMP)
-);
-
-CREATE TABLE "CREDIT_PURCHASE" (
-  "id" uuid PRIMARY KEY NOT NULL DEFAULT (gen_random_uuid()),
-  "user_id" uuid NOT NULL REFERENCES "USER"(id),
-  "credit_package_id" integer NOT NULL REFERENCES "CREDIT_PACKAGE"(id),
-  "purchased_credits" integer NOT NULL,
-  "price_paid" numeric(10,2) NOT NULL,
-  "created_at" timestamp NOT NULL DEFAULT (CURRENT_TIMESTAMP),
-  "purchase_at" timestamp NOT NULL DEFAULT (CURRENT_TIMESTAMP)
-);
-
-CREATE TABLE "COURSE" (
-  "id" serial PRIMARY KEY,
-  "user_id" uuid NOT NULL REFERENCES "USER"(id),
-  "skill_id" uuid NOT NULL REFERENCES "SKILL"(id),
-  "name" varchar(100) NOT NULL,
-  "description" text,
-  "start_at" timestamp NOT NULL,
-  "end_at" timestamp NOT NULL,
-  "max_participants" integer NOT NULL,
-  "meeting_url" varchar(2048) NOT NULL,
-  "created_at" timestamp NOT NULL DEFAULT (CURRENT_TIMESTAMP)
-);
-
-CREATE TABLE "COURSE_BOOKING" (
-  "id" uuid PRIMARY KEY NOT NULL DEFAULT (gen_random_uuid()),
-  "user_id" uuid NOT NULL REFERENCES "USER"(id),
-  "course_id" integer NOT NULL REFERENCES "COURSE"(id),
-  "booking_at" timestamp NOT NULL,
-  "status" varchar(20) NOT NULL,
-  "join_at" timestamp,
-  "leave_at" timestamp,
-  "cancelled_at" timestamp,
-  "cancellation_reason" varchar(255),
-  "created_at" timestamp NOT NULL DEFAULT (CURRENT_TIMESTAMP)
-);
-
-CREATE TABLE "BLOG_POST" (
-  "id" uuid PRIMARY KEY NOT NULL DEFAULT (gen_random_uuid()),
-  "user_id" uuid NOT NULL REFERENCES "USER"(id),
-  "title" varchar(255) NOT NULL,
-  "content" text NOT NULL,
-  "featured_image_url" varchar(2048),
-  "category" varchar(20) NOT NULL,
-  "spend_minutes" smallint NOT NULL,
-  "created_at" timestamp NOT NULL DEFAULT (CURRENT_TIMESTAMP),
-  "updated_at" timestamp NOT NULL DEFAULT (CURRENT_TIMESTAMP)
-);
-
-CREATE TABLE "COMMENT" (
-  "id" uuid PRIMARY KEY NOT NULL DEFAULT (gen_random_uuid()),
-  "blog_post_id" uuid NOT NULL REFERENCES "BLOG_POST"(id),
-  "user_id" uuid NOT NULL REFERENCES "USER"(id),
-  "content" text NOT NULL,
-  "created_at" timestamp NOT NULL DEFAULT (CURRENT_TIMESTAMP),
-  "updated_at" timestamp NOT NULL DEFAULT (CURRENT_TIMESTAMP)
-);
 -- 1. 新增：新增六筆用戶資料，資料如下：
 --     1. 用戶名稱為`李燕容`，Email 為`lee2000@hexschooltest.io`，Role為`USER`
 --     2. 用戶名稱為`王小明`，Email 為`wXlTq@hexschooltest.io`，Role為`USER`
@@ -148,29 +50,29 @@ SELECT * FROM "USER" LIMIT 3;
     -- 1. 名稱為 `7 堂組合包方案`，價格為`1,400` 元，堂數為`7`
     -- 2. 名稱為`14 堂組合包方案`，價格為`2,520` 元，堂數為`14`
     -- 3. 名稱為 `21 堂組合包方案`，價格為`4,800` 元，堂數為`21`
--- INSERT INTO "CREDIT_PACKAGE" (name, credit_amount, price) VALUES
--- ('7 堂組合包方案', 7, 1400),
--- ('14 堂組合包方案', 14, 2520),
--- ('21 堂組合包方案', 21, 4800);
+INSERT INTO "CREDIT_PACKAGE" (name, credit_amount, price) VALUES
+('7 堂組合包方案', 7, 1400),
+('14 堂組合包方案', 14, 2520),
+('21 堂組合包方案', 21, 4800);
 -- 2-2. 新增：在 `CREDIT_PURCHASE` 資料表，新增三筆資料：（請使用 name 欄位做子查詢）
     -- 1. `王小明` 購買 `14 堂組合包方案`
     -- 2. `王小明` 購買 `21 堂組合包方案`
     -- 3. `好野人` 購買 `14 堂組合包方案`
--- INSERT INTO "CREDIT_PURCHASE" (user_id, credit_package_id, purchased_credits, price_paid) VALUES
--- ((SELECT id FROM "USER" WHERE email = 'wXlTq@hexschooltest.io'),
---   (SELECT id FROM "CREDIT_PACKAGE" WHERE name = '14 堂組合包方案'),
---   (SELECT credit_amount FROM "CREDIT_PACKAGE" WHERE name = '14 堂組合包方案'),
---   (SELECT price FROM "CREDIT_PACKAGE" WHERE name = '14 堂組合包方案'));
--- INSERT INTO "CREDIT_PURCHASE" (user_id, credit_package_id, purchased_credits, price_paid) VALUES
--- ((SELECT id FROM "USER" WHERE email = 'wXlTq@hexschooltest.io'),
---   (SELECT id FROM "CREDIT_PACKAGE" WHERE name = '21 堂組合包方案'),
---   (SELECT credit_amount FROM "CREDIT_PACKAGE" WHERE name = '21 堂組合包方案'),
---   (SELECT price FROM "CREDIT_PACKAGE" WHERE name = '21 堂組合包方案'));
--- INSERT INTO "CREDIT_PURCHASE" (user_id, credit_package_id, purchased_credits, price_paid) VALUES
--- ((SELECT id FROM "USER" WHERE email = 'richman@hexschooltest.io'),
---   (SELECT id FROM "CREDIT_PACKAGE" WHERE name = '14 堂組合包方案'),
---   (SELECT credit_amount FROM "CREDIT_PACKAGE" WHERE name = '14 堂組合包方案'),
---   (SELECT price FROM "CREDIT_PACKAGE" WHERE name = '14 堂組合包方案'));
+INSERT INTO "CREDIT_PURCHASE" (user_id, credit_package_id, purchased_credits, price_paid) VALUES
+((SELECT id FROM "USER" WHERE email = 'wXlTq@hexschooltest.io'),
+  (SELECT id FROM "CREDIT_PACKAGE" WHERE name = '14 堂組合包方案'),
+  (SELECT credit_amount FROM "CREDIT_PACKAGE" WHERE name = '14 堂組合包方案'),
+  (SELECT price FROM "CREDIT_PACKAGE" WHERE name = '14 堂組合包方案'));
+INSERT INTO "CREDIT_PURCHASE" (user_id, credit_package_id, purchased_credits, price_paid) VALUES
+((SELECT id FROM "USER" WHERE email = 'wXlTq@hexschooltest.io'),
+  (SELECT id FROM "CREDIT_PACKAGE" WHERE name = '21 堂組合包方案'),
+  (SELECT credit_amount FROM "CREDIT_PACKAGE" WHERE name = '21 堂組合包方案'),
+  (SELECT price FROM "CREDIT_PACKAGE" WHERE name = '21 堂組合包方案'));
+INSERT INTO "CREDIT_PURCHASE" (user_id, credit_package_id, purchased_credits, price_paid) VALUES
+((SELECT id FROM "USER" WHERE email = 'richman@hexschooltest.io'),
+  (SELECT id FROM "CREDIT_PACKAGE" WHERE name = '14 堂組合包方案'),
+  (SELECT credit_amount FROM "CREDIT_PACKAGE" WHERE name = '14 堂組合包方案'),
+  (SELECT price FROM "CREDIT_PACKAGE" WHERE name = '14 堂組合包方案'));
 
 -- ████████  █████   █    ████   
 --   █ █   ██    █  █         ██ 
@@ -183,42 +85,47 @@ SELECT * FROM "USER" LIMIT 3;
     -- 1. 將用戶`李燕容`新增為教練，並且年資設定為2年（提示：使用`李燕容`的email ，取得 `李燕容` 的 `id` ）
     -- 2. 將用戶`肌肉棒子`新增為教練，並且年資設定為2年
     -- 3. 將用戶`Q太郎`新增為教練，並且年資設定為2年
--- INSERT INTO "COACH" (user_id, experience_years) VALUES 
--- ((SELECT id FROM "USER" WHERE email = 'lee2000@hexschooltest.io'), 2)
--- INSERT INTO "COACH" (user_id, experience_years) VALUES 
--- ((SELECT id FROM "USER" WHERE email = 'muscle@hexschooltest.io'), 2)
--- INSERT INTO "COACH" (user_id, experience_years) VALUES 
--- ((SELECT id FROM "USER" WHERE email = 'starplatinum@hexschooltest.io'), 2)
+INSERT INTO "COACH" (user_id, experience_years) VALUES 
+((SELECT id FROM "USER" WHERE email = 'lee2000@hexschooltest.io'), 2)
+INSERT INTO "COACH" (user_id, experience_years) VALUES 
+((SELECT id FROM "USER" WHERE email = 'muscle@hexschooltest.io'), 2)
+INSERT INTO "COACH" (user_id, experience_years) VALUES 
+((SELECT id FROM "USER" WHERE email = 'starplatinum@hexschooltest.io'), 2)
 
 -- 3-2. 新增：承1，為三名教練新增專長資料至 `COACH_LINK_SKILL` ，資料需求如下：
     -- 1. 所有教練都有 `重訓` 專長
     -- 2. 教練`肌肉棒子` 需要有 `瑜伽` 專長
     -- 3. 教練`Q太郎` 需要有 `有氧運動` 與 `復健訓練` 專長
 -- 插入教練技能關聯
--- INSERT INTO "COACH_LINK_SKILL" (coach_id, skill_id) VALUES (
---   (SELECT id FROM "COACH" WHERE user_id = (SELECT id FROM "USER" WHERE email = 'lee2000@hexschooltest.io')),
---   (SELECT id FROM "SKILL" WHERE name = '重訓')
--- );
+-- 為成為教練的用戶建立 COACH 記錄
+INSERT INTO "COACH" (user_id, experience_years) VALUES 
+((SELECT id FROM "USER" WHERE email = 'lee2000@hexschooltest.io'), 0),
+((SELECT id FROM "USER" WHERE email = 'muscle@hexschooltest.io'), 0),
+((SELECT id FROM "USER" WHERE email = 'starplatinum@hexschooltest.io'), 0);
+INSERT INTO "COACH_LINK_SKILL" (coach_id, skill_id) VALUES (
+  (SELECT id FROM "COACH" WHERE user_id = (SELECT id FROM "USER" WHERE email = 'lee2000@hexschooltest.io')),
+  (SELECT id FROM "SKILL" WHERE name = '重訓')
+);
 
--- INSERT INTO "COACH_LINK_SKILL" (coach_id, skill_id) VALUES (
---   (SELECT id FROM "COACH" WHERE user_id = (SELECT id FROM "USER" WHERE email = 'muscle@hexschooltest.io')),
---   (SELECT id FROM "SKILL" WHERE name = '重訓')
--- );
+INSERT INTO "COACH_LINK_SKILL" (coach_id, skill_id) VALUES (
+  (SELECT id FROM "COACH" WHERE user_id = (SELECT id FROM "USER" WHERE email = 'muscle@hexschooltest.io')),
+  (SELECT id FROM "SKILL" WHERE name = '重訓')
+);
 
--- INSERT INTO "COACH_LINK_SKILL" (coach_id, skill_id) VALUES (
---   (SELECT id FROM "COACH" WHERE user_id = (SELECT id FROM "USER" WHERE email = 'starplatinum@hexschooltest.io')),
---   (SELECT id FROM "SKILL" WHERE name = '重訓')
--- );
+INSERT INTO "COACH_LINK_SKILL" (coach_id, skill_id) VALUES (
+  (SELECT id FROM "COACH" WHERE user_id = (SELECT id FROM "USER" WHERE email = 'starplatinum@hexschooltest.io')),
+  (SELECT id FROM "SKILL" WHERE name = '重訓')
+);
 
--- INSERT INTO "COACH_LINK_SKILL" (coach_id, skill_id) VALUES (
---   (SELECT id FROM "COACH" WHERE user_id = (SELECT id FROM "USER" WHERE email = 'muscle@hexschooltest.io')),
---   (SELECT id FROM "SKILL" WHERE name = '瑜伽')
--- );
+INSERT INTO "COACH_LINK_SKILL" (coach_id, skill_id) VALUES (
+  (SELECT id FROM "COACH" WHERE user_id = (SELECT id FROM "USER" WHERE email = 'muscle@hexschooltest.io')),
+  (SELECT id FROM "SKILL" WHERE name = '瑜伽')
+);
 
--- INSERT INTO "COACH_LINK_SKILL" (coach_id, skill_id) VALUES (
---   (SELECT id FROM "COACH" WHERE user_id = (SELECT id FROM "USER" WHERE email = 'starplatinum@hexschooltest.io')),
---   (SELECT id FROM "SKILL" WHERE name = '有氧運動')
--- );
+INSERT INTO "COACH_LINK_SKILL" (coach_id, skill_id) VALUES (
+  (SELECT id FROM "COACH" WHERE user_id = (SELECT id FROM "USER" WHERE email = 'starplatinum@hexschooltest.io')),
+  (SELECT id FROM "SKILL" WHERE name = '有氧運動')
+);
 
 INSERT INTO "COACH_LINK_SKILL" (coach_id, skill_id) VALUES (
   (SELECT id FROM "COACH" WHERE user_id = (SELECT id FROM "USER" WHERE email = 'starplatinum@hexschooltest.io')),
@@ -227,21 +134,21 @@ INSERT INTO "COACH_LINK_SKILL" (coach_id, skill_id) VALUES (
 -- 3-3 修改：更新教練的經驗年數，資料需求如下：
     -- 1. 教練`肌肉棒子` 的經驗年數為3年
     -- 2. 教練`Q太郎` 的經驗年數為5年
--- update "COACH" 
--- set experience_years = 3 
--- WHERE user_id = (
---     SELECT id 
---     FROM "USER" 
---     WHERE email = 'muscle@hexschooltest.io');
--- update "COACH" 
--- set experience_years = 3 
--- WHERE user_id = (
---     SELECT id 
---     FROM "USER" 
---     WHERE email = 'starplatinum@hexschooltest.io');
+update "COACH" 
+set experience_years = 3 
+WHERE user_id = (
+    SELECT id 
+    FROM "USER" 
+    WHERE email = 'muscle@hexschooltest.io');
+update "COACH" 
+set experience_years = 3 
+WHERE user_id = (
+    SELECT id 
+    FROM "USER" 
+    WHERE email = 'starplatinum@hexschooltest.io');
 -- 3-4 刪除：新增一個專長 空中瑜伽 至 SKILL 資料表，之後刪除此專長。
--- INSERT INTO "SKILL" (name) VALUES ('空中瑜伽');
--- delete FROM "SKILL" WHERE name = '空中瑜伽';
+INSERT INTO "SKILL" (name) VALUES ('空中瑜伽');
+delete FROM "SKILL" WHERE name = '空中瑜伽';
 
 
 --  ████████  █████   █    █   █ 
@@ -260,23 +167,23 @@ INSERT INTO "COACH_LINK_SKILL" (coach_id, skill_id) VALUES (
     -- 5. 授課結束時間`end_at`設定為2024-11-25 16:00:00
     -- 6. 最大授課人數`max_participants` 設定為10
     -- 7. 授課連結設定`meeting_url`為 https://test-meeting.test.io
--- INSERT INTO "COURSE" (
---   user_id, 
---   skill_id, 
---   name, 
---   start_at, 
---   end_at, 
---   max_participants, 
---   meeting_url
--- ) VALUES (
---   (SELECT id FROM "USER" WHERE email = 'lee2000@hexschooltest.io'),
---   (SELECT id FROM "SKILL" WHERE name = '重訓'),
---   '重訓基礎課',
---   '2024-11-25 14:00:00',
---   '2024-11-25 16:00:00',
---   10,
---   'https://test-meeting.test.io'
--- );
+INSERT INTO "COURSE" (
+  user_id, 
+  skill_id, 
+  name, 
+  start_at, 
+  end_at, 
+  max_participants, 
+  meeting_url
+) VALUES (
+  (SELECT id FROM "USER" WHERE email = 'lee2000@hexschooltest.io'),
+  (SELECT id FROM "SKILL" WHERE name = '重訓'),
+  '重訓基礎課',
+  '2024-11-25 14:00:00',
+  '2024-11-25 16:00:00',
+  10,
+  'https://test-meeting.test.io'
+);
 
 -- ████████  █████   █    █████ 
 --   █ █   ██    █  █     █     
